d7457ca7d4740746523421937278a5ddb57ad00e
NyroY64 <youcef.meradi09@gmail.com>
2024-09-26 14:11:45 +0200

    -mise en place d'un fichier config example -inclusion du json -lecture du fichier json et extraction du dbconfig

--- DBConfig.py
+++ DBConfig.py
@@ -1,3 +1,5 @@
+import json
+
 class DBConfig:
     def __init__(self, dbpath):
         self.dbpath = dbpath
@@ -6,17 +8,23 @@ class DBConfig:
     def load_db_config(file_path):
         config=None
         
-        try:
-            file = open(file_path, 'r')
-            config=file.read()
-            print(config)
-            for line in file:
-                if line.startswith('dbpath'):
-                    _, path = line.split('=')
-                    config= DBConfig(dbpath=path.strip().strip("\""))
-                    return config
-        finally:
-            file.close()
+        with open(file_path, 'r') as file:
+            config=json.load(file)
+            config= config["dbms_settings"]["dbpath"]
+            return config
+            
+        
+        # try:
+        #     file = open(file_path, 'r')
+        #     config=file.read()
+        #     print(config)
+        #     for line in file:
+        #         if line.startswith('dbpath'):
+        #             _, path = line.split('=')
+        #             config= DBConfig(dbpath=path.strip().strip("\""))
+        #             return config
+        # finally:
+        #     file.close()
          
         
 
