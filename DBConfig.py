31b91b87a5b179c3bf17e3000dbeace0a7ece4cd
Ramzy <115043043+DZ-Ramzy@users.noreply.github.com>
2024-10-03 17:33:57 +0200

    Update DBConfig.py
    editing thee code

--- DBConfig.py
+++ DBConfig.py
@@ -26,20 +26,5 @@ class DBConfig:
             
         except Exception as openFail:
             print(f"erreur d'ouverture de fichier = {openFail}")
-
-            
-        
-        # try:
-        #     file = open(file_path, 'r')
-        #     config=file.read()
-        #     print(config)
-        #     for line in file:
-        #         if line.startswith('dbpath'):
-        #             _, path = line.split('=')
-        #             config= DBConfig(dbpath=path.strip().strip("\""))
-        #             return config
-        # finally:
-        #     file.close()
-         
         
 
