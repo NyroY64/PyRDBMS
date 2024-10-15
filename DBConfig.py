850f96a2d1bec374e62037c0af881702f9ae90fd
DERMOUCHE RYAD <95882954+DERMOUCHERYAD@users.noreply.github.com>
2024-10-15 18:46:29 +0200

    Update DBConfig.py
    adding la  methode rep pour la representation officelle de l'objet

--- DBConfig.py
+++ DBConfig.py
@@ -6,6 +6,14 @@ class DBConfig:
         self.dbpath = dbpath
         self.pageSize=pageSize
         self.dm_maxfilesize=dm_maxfilesize
+
+      # Méthode __repr__ pour la représentation en chaîne de l'objet
+    def __repr__(self):
+        """
+        Représentation de l'objet DBConfig, affichant les informations de configuration.
+        """
+        return f"DBConfig(dbpath='{self.dbpath}', pageSize={self.pageSize}, dm_maxfilesize={self.dm_maxfilesize})"
+
     
 
     def get_dbpath(self):
@@ -13,6 +21,18 @@ class DBConfig:
 
     def set_dbpath(self, dbpath):
         self.dbpath = dbpath
+
+     def get_pageSize(self):
+        return self.pageSize
+
+    def set_pageSize(self, pageSize):
+        self.pageSize = pageSize
+
+    def get_dm_maxfilesize(self):
+        return self.dm_maxfilesize
+
+    def set_dm_maxfilesize(self, dm_maxfilesize):
+        self.dm_maxfilesize = dm_maxfilesize
         
         #Le type de méthode LoadDBConfig : Vous l'avez définie comme une méthode classique de la classe, mais elle n'utilise pas l'instance (self).
         #Cela signifie qu'elle devrait être définie comme une méthode statique (ou classe) en utilisant @staticmethod.......
