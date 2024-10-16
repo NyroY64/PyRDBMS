eb63a7bcc9a799bdd5ee81b3220f80fdcbd296da
DERMOUCHE RYAD <95882954+DERMOUCHERYAD@users.noreply.github.com>
2024-10-16 14:38:55 +0200

    Update pageId.py

--- pageId.py
+++ pageId.py
@@ -2,4 +2,22 @@ class pageId:
     def __init__(self, FileIdx, PageIdx ):
       self.FileIdx = FileIdx
       self.PageIdx = PageIdx
-      
+
+    def __repr__(self):
+        """
+        Représentation de l'objet PageId pour l'affichage.
+        """
+        return f"PageId(FileIdx={self.FileIdx}, PageIdx={self.PageIdx})"
+
+    def get_FileIdx(self):
+        return self.FileIdx
+
+    def set_FileIdx(self, FileIdx):
+       self.FileIdx = FileIdx
+
+    
+    def get_PageIdx(self):
+       return self.PageIdx
+        
+    def set_page_idx(self, PageIdx):
+         self.PageIdx = PageIdx
