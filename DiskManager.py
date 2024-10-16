68c5a9de891ddd4892c488f7a589ac239be9df25
DERMOUCHE RYAD <95882954+DERMOUCHERYAD@users.noreply.github.com>
2024-10-16 14:49:05 +0200

    Update DiskManager.py

--- DiskManager.py
+++ DiskManager.py
@@ -1,3 +1,9 @@
 class DiskManager:
-    def __init__(self, dbc):
-      self.dbc=dbc
+    def __init__(self, (db_config):
+      self.db_config=db_config
+
+     def __repr__(self):
+        """
+        Représentation de l'objet DiskManager pour l'affichage.
+        """
+        return f"DiskManager(db_config={self.db_config})"
