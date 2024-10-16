d5a587b297e704795c04c0ab1be4e2349515676c
DERMOUCHE RYAD <95882954+DERMOUCHERYAD@users.noreply.github.com>
2024-10-16 14:21:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from DBConfig import DBConfig
 if __name__ == "__main__":
     config = DBConfig.load_db_config('config.txt')
     print(config.get_dbpath())
+    print(config.get_pageSize())
+    print(config.get_dm_maxfilesize())
+    
 
 #T'as rien testé YOUCEFF t'as juste récupérer le chemin haha depuis un fichier.txt alors qu'on a chiisi deja un fichier.json pour la config !!!
+#Je parle des erreurs et pas un simple tests ? 
