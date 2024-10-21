20e189dbccefbba6ad6b8f9683de23738a7e642b
NyroY64 <youcef.meradi09@gmail.com>
2024-10-21 20:24:40 +0200

    -updated the config file to test the DiskManager -updated the pageId.py syntax

--- pageId.py
+++ pageId.py
@@ -1,5 +1,7 @@
-class pageId:
-    def __init__(self, FileIdx, PageIdx ):
-      self.FileIdx = FileIdx
-      self.PageIdx = PageIdx
-      
+class PageId:
+    def __init__(self, FileIdx, PageIdx):
+        self.FileIdx = FileIdx
+        self.PageIdx = PageIdx
+    
+    def __repr__(self):
+        return f"PageId(FileIdx={self.FileIdx}, PageIdx={self.PageIdx})"
